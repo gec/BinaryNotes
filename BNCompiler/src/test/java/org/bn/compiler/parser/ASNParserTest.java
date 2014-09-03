4501810795c936d8c90c55c3a34606c4beaccd2f
Pavel Drasil <pavel.drasil@gmail.com>
2014-09-03 14:46:09 +0200

    Fixed failing test

--- BNCompiler/src/test/java/org/bn/compiler/parser/ASNParserTest.java
+++ BNCompiler/src/test/java/org/bn/compiler/parser/ASNParserTest.java
@@ -62,11 +62,11 @@ public class ASNParserTest extends TestCase {
     public void testModule_definition() throws Exception {
         ASN1Model model = createFromStream();
 
-        assertEquals(model.module.moduleIdentifier.name, "TEST_ASN");
-        assertEquals(model.module.asnTypes.sequenceSets.size(), 17);
-        assertEquals(model.module.asnTypes.enums.size(), 2);
-        assertEquals(model.module.asnTypes.characterStrings.size(), 6);
-        assertEquals(model.module.asnTypes.octetStrings.size(), 1);
-        assertEquals(model.module.asnTypes.sequenceSetsOf.size(), 7);
+        assertEquals("TEST_ASN", model.module.moduleIdentifier.name);
+        assertEquals(20, model.module.asnTypes.sequenceSets.size());
+        assertEquals(2, model.module.asnTypes.enums.size());
+        assertEquals(7, model.module.asnTypes.characterStrings.size());
+        assertEquals(1, model.module.asnTypes.octetStrings.size());
+        assertEquals(7, model.module.asnTypes.sequenceSetsOf.size());
     }
 }
